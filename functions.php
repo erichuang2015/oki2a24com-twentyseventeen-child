2315783baa0552bfcd7493a6a9162353951896bf
oki2a24 <oki2a24@gmail.com>
2016-12-13 06:29:47 +0900

    more タグで URL 末端に付く more-xxxx を削除する

--- functions.php
+++ functions.php
@@ -7,3 +7,12 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+/**
+ * more タグで URL 末端に付く #more-xxxx を削除します。
+ */
+function remove_more_link_scroll( $link ) {
+        $link = preg_replace( '|#more-[0-9]+|', '', $link );
+        return $link;
+}
+add_filter( 'the_content_more_link', 'remove_more_link_scroll' );
